aedf7cb28bfb54e4a24a62d4aa48d9368ba3b98a
Wagner Pires Ferreira <wagner_pires@icloud.com>
2024-08-15 00:03:42 -0600

    simple app

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-function Add(e){
-    const element = document.getElementById('inputBox')
-    const list = document.getElementById('list');
+function OnLoad(e) {
+  if (!localStorage.getItem("list")) {
+    return;
+  }
+  let listItems = [];
+  listItems = JSON.parse(localStorage.getItem("list"));
+  const list = document.getElementById("list");
+  for (let i = 0; i < listItems.length; i++) {
     const node = document.createElement("li");
-    node.innerText = element.value;
-    node.id = element.value
-    
-    const removeButton = document.createElement('button')
-    removeButton.innerText = 'X'
+    node.innerText = listItems[i];
+    node.id = listItems[i];
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "X";
     removeButton.addEventListener("click", remove);
-    
     node.appendChild(removeButton);
     list.appendChild(node);
+  }
 }
 
-function remove(){
-    const list = document.getElementById('list');
-    console.log(this.parentElement)
-    list.removeChild(this.parentElement)
+function Add(e) {
+  const element = document.getElementById("inputBox");
+  const list = document.getElementById("list");
+  const node = document.createElement("li");
+  node.innerText = element.value;
+  node.id = element.value;
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "X";
+  removeButton.addEventListener("click", remove);
+  node.appendChild(removeButton);
+  list.appendChild(node);
+  let listOfItems = [];
+  if (localStorage.getItem("list")) {
+    listOfItems = JSON.parse(localStorage.getItem("list"));
+  }
+  listOfItems.push(element.value);
+  localStorage.setItem("list", JSON.stringify(listOfItems));
+}
+
+function remove() {
+  const list = document.getElementById("list");
+  list.removeChild(this.parentElement);
+  const listItems = JSON.parse(localStorage.getItem("list"));
+  let result = [];
+  result = listItems.filter((item) => item !== this.parentElement.id);
+  localStorage.setItem("list", JSON.stringify(result));
 }
